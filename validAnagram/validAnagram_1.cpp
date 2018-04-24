bdac8d37e06bf0cfc8ad26735a1f273dea898e00
evelyn-plee <evelyn.plee@gmail.com>
2018-04-24 22:24:21 +1200

    validAnagram

---
+++ validAnagram/validAnagram_1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length()) return false;
+
+        vector<int> charcount(26, 0);
+        for(int i = 0 ; i < s.length(); i++){
+            charcount[s[i] - 'a']++;
+        }
+        for(int i = 0 ; i< t.length(); i++){
+            charcount[t[i] - 'a'] --;
+        }
+        for(int i = 0; i< charcount.size(); i++){
+            if(charcount[i] != 0) return false;
+        }
+        return true;
+    }
+};
