9f662fd315a67583105b230485a3a25de2f4f250
evelyn-plee <evelyn.plee@gmail.com>
2018-06-02 17:11:23 +1200

    maxDepth

---
+++ maxDepthBinaryTree/maxDepthBinaryTree3.cpp
@@ -0,0 +1,19 @@
+class Solution{
+    public:
+        int maxDepth(TreeNode* root){
+            if (!root) return 0;
+            int res = 0;
+            queue<TreeNode*> q;
+            q.push(root);
+            while(!q.empty()){
+                ++res;
+                int n = q.size();
+                for(int i = 0; i < n; ++i){
+                    TreeNode *t = q.front(); q.pop();
+                    if(t->left) q.push(t->left);
+                    if(t->right) q.push(t->right);
+                }
+            }
+            return res;
+        }
+};
