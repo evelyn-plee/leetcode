069a4d16519fd2d3a0ddcec0ad6fb3ba0379b235
evelyn-plee <evelyn.plee@gmail.com>
2018-06-20 21:41:48 +1200

    trapRainWater

---
+++ trappingRainWater/trappingRainWater-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int res = 0, mx = 0, n = height.size();
+        vector<int> dp(n, 0);
+        for (int i = 0; i < n; ++i) {
+            dp[i] = mx;
+            mx = max(mx, height[i]);
+        }
+        mx = 0;
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i] = min(dp[i], mx);
+            mx = max(mx, height[i]);
+            if (dp[i] > height[i]) res += dp[i] - height[i];
+        }
+        return res;
+    }
+};
