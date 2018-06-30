68d7c9e58b30d705fbaa253dc9d4c915b8bfd91f
evelyn-plee <evelyn.plee@gmail.com>
2018-06-30 20:24:26 +1200

    palindromeLinkedList

---
+++ palindromeLinkedList/palindromeLinkedList1.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode{
+ *      int val;
+ *      ListNode *next;
+ *      ListNode(int x) : val(x), next(NULL){}
+ * };
+ */
+#include <ListNode>
+
+class Solution{
+    public:
+    bool isPalindrome(ListNode* head){
+        if(!head || !head->next) return true;
+        ListNode *slow = head, *fast = head;
+        stack<int> s;
+        s.push(head->val);
+        while(fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            s.push(slow->val);
+        }
+        if (!fast->next) s.pop();
+        while(slow->next){
+            slow = slow->next;
+            int temp = s.top(); s.pop();
+            if(temp != slow->val) return false;
+        }
+        return true;
+    }
+};
