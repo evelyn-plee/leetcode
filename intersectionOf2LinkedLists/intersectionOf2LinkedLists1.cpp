c3451f0f95987c08fbcffcce52613cb47a9e998b
evelyn-plee <evelyn.plee@gmail.com>
2018-07-01 18:34:08 +1200

    linkedLists

---
+++ intersectionOf2LinkedLists/intersectionOf2LinkedLists1.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+#include <iostream>
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        if (!headA || !headB) return NULL;
+        ListNode *a = headA, *b = headB;
+        while (a != b) {
+            a = a ? a->next : headB;
+            b = b ? b->next : headA;
+        }
+        return a;
+    }
+};
+复制代码
