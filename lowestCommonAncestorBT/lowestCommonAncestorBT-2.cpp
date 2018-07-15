bcffeeec4557083b593150d491f6b3b6847b712f
evelyn-plee <evelyn.plee@gmail.com>
2018-07-15 20:18:13 +1200

    lowest common ancestor

---
+++ lowestCommonAncestorBT/lowestCommonAncestorBT-2.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+#include<iostream>
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root || p == root || q == root) return root;
+        TreeNode *left = lowestCommonAncestor(root->left, p, q);
+        if(left && left != p && left != q) return left;
+        TreeNode *right = lowestCommonAncestor(root->right, p, q);
+        if(right && right != p && right !=q) return right;
+        if(left && right) return root;
+        return left ? left : right;
+    }
+};
