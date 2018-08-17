10b93151ddbb41a900cc48b049f4ed8d6399a4fe
evelyn-plee <evelyn.plee@gmail.com>
2018-08-18 10:00:08 +1200

    Create kthLargestElementInArray_2.cpp

---
+++ kthLargestElementInArray/kthLargestElementInArray_2.cpp
@@ -0,0 +1,10 @@
+class Solution{
+public:
+    int findKthLargest(vector<int>& nums, int k){
+        priority_queue<int> q(nums.begin(), nums.end());
+        for(int i = 0; i < k - 1; ++i){
+            q.pop();
+        }
+        return q.top();
+    }
+};
