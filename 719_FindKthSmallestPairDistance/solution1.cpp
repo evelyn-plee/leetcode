66c51ef19d7e0730c8cd1fe7b883c24eed70bd43
evelyn-plee <evelyn.plee@gmail.com>
2018-09-28 20:30:37 +1200

    Create solution1.cpp

---
+++ 719_FindKthSmallestPairDistance/solution1.cpp
@@ -0,0 +1,29 @@
+/*
+using bucket sort/ brute force
+Time complexity: O(n^2)
+Space complexity: O(max(nums))
+*/
+
+
+#include<vector>
+
+class Solution{
+public:
+    int smallestDistancePair(vector<int>& nums, int k){
+        sort(nums.begin(), nums.end());
+        const int N = nums.back();
+        const int n = nums.size();
+        vector<int> cnt(N+1, 0);
+        for(int i = 0; i < n; ++i){
+            for(int j = i+1; j < n; ++j){
+                ++cnt[nums[j] - nums[i]];
+            }
+        }
+        
+        for(int i = 0; i < N; ++i){
+            k -= cnt[i];
+            if(k <= 0) return i;
+        }
+        return 0;
+    }
+};
