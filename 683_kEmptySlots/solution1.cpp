f7a8ab2e2b30f74ab3819432250418bca29367fa
evelyn-plee <evelyn.plee@gmail.com>
2018-10-03 21:34:50 +1300

    Update solution1.cpp

--- 683_kEmptySlots/solution1.cpp
+++ 683_kEmptySlots/solution1.cpp
@@ -1,34 +1,32 @@
+/*
+Time Complexity: O(n)
+Space Complexity: O(n), to store which flower is open on each day
+*/
+
 #include <vector>
 #include <math>
 
 using namespace std;
 
-class Solution{
+class Solution {
 public:
     int kEmptySlots(vector<int>& flowers, int k){
-        int n = flowers.size();
-        if(n == 0 || k >= n) return -1;
-        
-        vector<int> day(n, 0);
-        for(int i = 0; i < n; ++i){
-            day[flowers[i]-1] = i+1;
-        }
-        
-        int itr = 0, res = INT_MAX;
-        while(itr < n - k - 1){
-            int tmp = max(day[itr], day[itr+k+1]);
-            int end = itr + k + 1;
-            for(int i = itr; i < end; i++){
-                if(day[i] < tmp){
-                    itr = i;
-                    break;
-                }
-            }
-            if(itr + k + 1 == end){
-                res = min(res, tmp);
-                itr++;
-            }
-        }
-        return res == INT_MAX ? -1 : res;
+       int n = flowers.size();
+       if(n == 0 || k >= n) return -1;
+       
+       vector<int> day(n, 0);
+       for(int i = 0; i < n; i++){
+           day[flowers[i]-1] = i+1;
+       }
+
+      int l = 0, r = k + 1, res = INT_MAX;
+      for(int i = 0; r < n; i++){
+          if(day[i] < day[l] || day[i] <= day[r]){
+              if(i == r) res = min(res, max(day[l], day[r]));
+              l = i;
+              r = i + k + 1;
+          }
+      }
+       return res == INT_MAX ? -1 : res;
     }
 };
