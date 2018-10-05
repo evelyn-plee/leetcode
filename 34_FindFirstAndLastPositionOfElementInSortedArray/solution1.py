84be6fa5dc480af7b68a5da9867d1c4e6a2dcbe8
evelyn-plee <evelyn.plee@gmail.com>
2018-10-06 12:15:56 +1300

    Update solution1.py

--- 34_FindFirstAndLastPositionOfElementInSortedArray/solution1.py
+++ 34_FindFirstAndLastPositionOfElementInSortedArray/solution1.py
@@ -1,19 +1,25 @@
-def searchRange(self, nums, target):
-    def binarySearchLeft(A, x):
-        left, right = 0, len(A) - 1
-        while left <= right:
-            mid = (left + right) / 2
-            if x > A[mid]: left = mid + 1
-            else: right = mid - 1
-        return left
-
-    def binarySearchRight(A, x):
-        left, right = 0, len(A) - 1
-        while left <= right:
-            mid = (left + right) / 2
-            if x >= A[mid]: left = mid + 1
-            else: right = mid - 1
-        return right
-        
-    left, right = binarySearchLeft(nums, target), binarySearchRight(nums, target)
-    return (left, right) if left <= right else [-1, -1]
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        l, r = 0, len(nums) - 1
+        notFound = True
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] > target:
+                r = m - 1
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                l, r = m, m
+                while l > 0 and target == nums[l-1]:
+                    l -= 1
+                while r < len(nums) - 1 and target == nums[r+1]:
+                    r += 1
+                notFound = false
+                break
+         if notFound is True: return [-1, -1]
+         else: return [l,r]
