85ce4543a123057b1dc6e3574f76ae3b07f04aa7
evelyn-plee <evelyn.plee@gmail.com>
2018-10-09 07:41:20 +1300

    Create solution2.cpp

---
+++ 307_RangeSumQuery_Mutable/solution2.cpp
@@ -0,0 +1,40 @@
+class NumArray {
+private:
+    vector<int> num;
+    vector<int> bit;
+public:
+    NumArray(vector<int> nums) {
+        num.resize(nums.size() + 1);
+        bit.resize(nums.size() + 1);
+        for (int i = 0; i < nums.size(); ++i) {
+            update(i, nums[i]);
+        }
+    }
+    
+    void update(int i, int val) {
+        int diff = val - num[i + 1];
+        for (int j = i + 1; j < num.size(); j += (j&-j)) {
+            bit[j] += diff;
+        }
+        num[i + 1] = val;
+    }
+    
+    int sumRange(int i, int j) {
+        return getSum(j + 1) - getSum(i);
+    }
+    
+    int getSum(int i) {
+        int res = 0;
+        for (int j = i; j > 0; j -= (j&-j)) {
+            res += bit[j];
+        }
+        return res;
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * obj.update(i,val);
+ * int param_2 = obj.sumRange(i,j);
+ */
