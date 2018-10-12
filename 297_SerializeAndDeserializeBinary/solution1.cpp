7dcf6748e32d73cf543d44657fa39b75e9835a65
evelyn-plee <evelyn.plee@gmail.com>
2018-10-12 20:13:08 +1300

    Create solution1.cpp

---
+++ 297_SerializeAndDeserializeBinary/solution1.cpp
@@ -0,0 +1,35 @@
+class Codec{
+public:
+    // Encodes a tree to a single string
+    string serialize(TreeNode* root){
+        ostringstream out;
+        serialize(root, out);
+        return out.str();
+    }
+    
+    // Decodes your encoded data to a tree
+    TreeNode* deserialize(string data){
+        istringstream in(data);
+        return deserialize(in);
+    }
+private:
+    void serialize(TreeNode* root, ostringstream& out){
+        if(!root){
+            out << "# "; 
+            return;
+        }
+        out << root->val << " ";
+        serialize(root->left);
+        serialize(root->right);
+    }
+    
+    TreeNode* deserialize(istringstream in){
+        string val;
+        in >> val;
+        if(val == "#") return nullptr;
+        TreeNode* root = new TreeNode(stoi(in));
+        root->left = deserialize(in);
+        root->right = deserialize(in);
+        return root;
+    }
+};
