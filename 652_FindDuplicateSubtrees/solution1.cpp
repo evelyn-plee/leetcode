3f7f844498be6d347472fdd788a211131ce564c4
evelyn-plee <evelyn.plee@gmail.com>
2018-10-13 19:28:08 +1300

    Create solution1.cpp

---
+++ 652_FindDuplicateSubtrees/solution1.cpp
@@ -0,0 +1,31 @@
+/** Time complexity: O(n^2); Space complexity: O(n^2) **/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<TreeNode*> findDuplicateSubtrees(TreeNode* root) {
+        unordered_map<string, int> counts;
+        vector<TreeNode*> ans;
+        serialize(root, counts, ans);
+        return ans;
+    }
+private:
+    string serialize(TreeNode* root, unordered_map<string, int>& counts, vector<TreeNode*>& ans){
+        if(!root) return "#";
+        string key = to_string(root->val) + "," 
+                     + serialize(root->left, counts, ans) + ","
+                     + serialize(root->right, counts, ans);
+        if(++counts[key] == 2){
+            ans.push_back(root);
+        }
+        return key;
+    }
+};
