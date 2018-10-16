8599696f170de76545ee2826039506155196ef37
evelyn-plee <evelyn.plee@gmail.com>
2018-10-16 19:48:50 +1300

    Create solution1.cpp

---
+++ 562_LongestLineOfConsecutiveOneInMatrix/solution1.cpp
@@ -0,0 +1,58 @@
+// Brute Force: Time complexity, O(n), Space complexity, O(1)
+
+class Solution {
+public:
+    int longestLine(vector<vector<int>>& M) {
+        int m = M.size(), n = m ? M[0].size():0;
+        int maxLine = 0, line = 0; 
+        for(int i = 0; i < m; ++i){
+            line = 0; 
+            for(int j = 0; j < n; ++j){
+                line = M[i][j] ? line+1:0;
+                maxLine = maxLine > line ? maxLine:line; 
+            }
+        }
+        
+        for(int j = 0; j < n; ++j){
+            line = 0;
+            for(int i = 0; i < m; ++i){
+                line = M[i][j] ? line+1:0;
+                maxLine = maxLine > line ? maxLine:line; 
+            }
+        }
+        
+        for(int i = 0; i < m; ++i){
+            line = 0;
+            for(int k = i, j = 0; k < m && j < n; ++k, ++j){
+                line = M[k][j] ? line+1:0;
+                maxLine = maxLine > line ? maxLine:line; 
+            }
+        }
+        
+        for(int i = 0; i < n; ++i){
+            line = 0;
+            for(int k = 0, j = i; k < m && j < n; ++k, ++j){
+                line = M[k][j] ? line+1:0;
+                maxLine = maxLine > line ? maxLine:line; 
+            }
+        }
+        
+        for(int i = 0; i < m; ++i){
+            line = 0;
+            for(int k = i, j = 0; k >= 0 && j < n; --k, ++j){
+                line = M[k][j] ? line+1:0;
+                maxLine = maxLine > line ? maxLine:line; 
+            }
+        }
+        
+        for(int i = 0; i < n; ++i){
+            line = 0;
+            for(int k = m-1, j = i; k >= 0 && j < n; --k, ++j){
+                line = M[k][j] ? line+1:0;
+                maxLine = maxLine > line ? maxLine:line; 
+            }
+        }
+        
+        return maxLine;
+    }
+};
