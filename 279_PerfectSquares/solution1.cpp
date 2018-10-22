4d51121a5a21d7234ad69e5f6c1479fc121f8da1
evelyn-plee <evelyn.plee@gmail.com>
2018-10-22 16:12:55 +1300

    Update solution1.cpp

--- 279_PerfectSquares/solution1.cpp
+++ 279_PerfectSquares/solution1.cpp
@@ -12,3 +12,22 @@ public:
         return dp[n];
     }
 };
+
+
+class Solution2{
+public:
+    int numSquares(int n) {
+        int dp[n+1] = {INT_MAX};
+        dp[0] = 0;
+        for(int i = 1; i <= n; ++i){
+            int squares = INT_MAX;
+            int j = 1;
+            while(i - j*j >=0){
+                squares = min(squares, dp[i-j*j]+1);
+                ++j;
+            }
+            dp[i] = squares;
+        }
+        return dp[n];
+    }
+};
