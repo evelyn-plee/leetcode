792934446cb10f68312cb4c82ed5159d98a3d187
evelyn-plee <evelyn.plee@gmail.com>
2019-04-22 17:29:21 +1200

    Create main.cpp

---
+++ 701_InsertIntoABinarySearchTree/main.cpp
@@ -0,0 +1,16 @@
+// Time Complexity: O(log n) - O(n)
+// Space Complexity: O(log n) - O(n)
+
+class Solution{
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val){
+        if(root == nullptr){
+            return new TreeNode(val);
+        }
+        
+        if(root->val > val) root->left = insertIntoBST(root->left, val);
+        else root->right = insertIntoBST(root->right, val);
+        
+        return root;
+    }
+};
