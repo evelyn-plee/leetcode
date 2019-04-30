46f730e10ef30501aebb2222501e62b384a128b7
evelyn-plee <evelyn.plee@gmail.com>
2019-04-30 20:29:28 +1200

    Update main.cpp

--- 101_SymmetricTree/main.cpp
+++ 101_SymmetricTree/main.cpp
@@ -7,7 +7,8 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
- 
+
+// Recursive
 class Solution{
 public:
    bool isSymmetric(TreeNode* root){
@@ -22,3 +23,26 @@ private:
         return (p->val == q->val) && isMirror(p->left, q->right) && isMirror(p->right, q->left);
     }
 };
+
+
+// Iterative
+class Solution{
+public:
+    bool isSymmetric(TreeNode* root){
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(root);
+        while(!q.empty()){
+            TreeNode* t1 = q.front(); q.pop();
+            TreeNode* t2 = q.front(); q.pop();
+            if(!t1 && !t2) continue;
+            if(!t1 || !t2) return false;
+            if(t1->val != t2->val) return false;
+            q.push(t1->left);
+            q.push(t2->right);
+            q.push(t1->right);
+            q.push(t2->left);
+        }
+        return q.empty();
+    }
+}
