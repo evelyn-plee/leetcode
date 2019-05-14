a0dde423510e3a2151cb3e4ab5a60fe1e493b531
evelyn-plee <evelyn.plee@gmail.com>
2019-05-14 22:32:57 +1200

    Update main.java

--- 1020_NumberOfEnclaves/main.java
+++ 1020_NumberOfEnclaves/main.java
@@ -34,3 +34,35 @@ class DFS_Solution{
         return x >= 0 && y >= 0 && x < w && y < h;
     }
 }
+
+class BFS_Solution {
+    public int numEnclaves(int[][] A) {
+        int h = A.length; int w = A[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        int[][] dirs = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        for(int i = 0; i < w; i++){
+            if(A[0][i] == 1) q.add(new int[]{0, i});
+            if(A[h-1][i] == 1) q.add(new int[]{h-1, i});
+        }
+        for(int i = 1; i < h-1; i++){
+            if(A[i][0] == 1) q.add(new int[]{i, 0});
+            if(A[i][w-1] == 1) q.add(new int[]{i, w-1});
+        }
+        while(!q.isEmpty()){
+            int[] cur = q.poll(); int x = cur[1]; int y = cur[0];
+            if(x < 0 || y < 0 || x >= w || y >= h || A[y][x] == 0) continue;
+            A[y][x] = 0;
+            for(int[] dir : dirs){
+                int xx = x + dir[1], yy = y + dir[0];
+                q.add(new int[]{yy, xx});
+            }
+        }
+        int count = 0;
+        for(int i = 0; i < w; i++){
+            for(int j = 0; j < h; j++){
+                if(A[j][i] == 1) count += 1;
+            }
+        }
+        return count;
+    }
+}
